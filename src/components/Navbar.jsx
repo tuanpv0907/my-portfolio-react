9bbcdd8df5267d8eab4d7abd6f84bb087b888a50
Tuan Pham <tuanpv0907@gmail.com>
2022-10-02 13:20:53 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 text-white bg-black fixed px-4">
       <div>
-        <h1 className="text-3xl font-signature ml-2">MeoMongMer</h1>
+        <h1 className="text-3xl font-signature ml-2">TuanPv</h1>
       </div>
       <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
